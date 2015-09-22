e9dfe87fcd7947451719709dec7e76c677b016aa
Robert Northard <robertnorthard@googlemail.com>
2015-09-22 17:42:14 +0100

    Added chef source/issues URLs to metadata
    Signed-off-by: Robert Northard <robertnorthard@googlemail.com>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      "Install and configure the collectd monitoring daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 supports         "ubuntu"
+
+source_url 'hhttps://github.com/coderanger/chef-collectd' if respond_to?(:source_url)
+issues_url 'https://github.com/coderanger/chef-collectd/issues' if respond_to?(:issues_url)
